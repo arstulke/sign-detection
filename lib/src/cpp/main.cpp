30504afc273b78c0b3830f981e650c52c21a093e
arstulke <arne.stulken2000@gmail.com>
2023-03-26 21:00:00 +0200

    replacing custom grayscale implementation with OpenCV cvtColor

--- lib/src/cpp/main.cpp
+++ lib/src/cpp/main.cpp
@@ -7,6 +7,10 @@ using namespace cv;
 
 class Bitmap4C_t
 {
+    private:
+      Mat* mat;
+      bool freeMat;
+      bool freePtr;
     public:
       uintptr_t ptr;
       int32_t width;
@@ -14,14 +18,53 @@ class Bitmap4C_t
       int32_t byteLength;
       static const int32_t BYTES_PER_PIXEL = 4;
 
+      // TODO can this be private?
+      Bitmap4C_t() {
+        this->freeMat = false;
+        this->freePtr = false;
+      }
+
       Bitmap4C_t(int32_t width, int32_t height) {
         int32_t byteLength = width * height * BYTES_PER_PIXEL;
         uint8_t* outputArray = new uint8_t[byteLength];
         
+        this->freeMat = false;
+        this->freePtr = true;
         this->width = width;
         this->height = height;
         this->byteLength = byteLength;
         this->ptr = reinterpret_cast<uintptr_t>(outputArray);
+      }
+
+      Bitmap4C_t(Mat* mat) {
+        this->mat = mat;
+        this->freeMat = true;
+        this->freePtr = false;
+        this->width = mat->cols;
+        this->height = mat->rows;
+        this->byteLength = mat->total() * BYTES_PER_PIXEL;
+        this->ptr = reinterpret_cast<uintptr_t>(mat->data);
+      }
+
+      Mat createMat() {
+        uint8_t* arr = reinterpret_cast<uint8_t*>(this->ptr);
+        Mat* mat = new Mat(this->height, this->width, CV_8UC4, arr);
+
+        this->mat = mat;
+        this->freeMat = true;
+
+        return *mat;
+      }
+
+      void release() {
+        if (this->freeMat) {
+          this->mat->release();
+        }
+
+        if (this->freePtr) {
+          void* castedPtr = reinterpret_cast<void *>(this->ptr);
+          free(castedPtr);
+        }
       }
 };
 
@@ -31,44 +74,26 @@ struct Response_t
 };
 
 int customGarbageCount = 2;
-uintptr_t* customGarbage = new uintptr_t[customGarbageCount];
+Bitmap4C_t* customGarbage = new Bitmap4C_t[customGarbageCount];
 
-Response_t processFrame(Bitmap4C_t input) {
-    uint8_t* inputArray = reinterpret_cast<uint8_t*>(input.ptr);
+Response_t processFrame(Bitmap4C_t inputBitmap) {
+    Mat input = inputBitmap.createMat();
+    Mat gray, output;
+    cvtColor(input, gray, COLOR_RGBA2GRAY);
+    cvtColor(gray, output, COLOR_GRAY2RGBA);
 
-    Bitmap4C_t output(input.width, input.height);
-    uint8_t* outputArray = reinterpret_cast<uint8_t*>(output.ptr);
+    Response_t response = { /*output=*/ Bitmap4C_t(&output) };
 
-    for (int i = 0; i < input.byteLength; i+=4) {
-      int r = inputArray[i + 0];
-      int g = inputArray[i + 1];
-      int b = inputArray[i + 2];
-      int a = inputArray[i + 3];
-      int avg = (r+g+b) / 3;
-      outputArray[i + 0] = avg;
-      outputArray[i + 1] = avg;
-      outputArray[i + 2] = avg;
-      outputArray[i + 3] = a;
-    }
-
-    Response_t response = { /*output=*/ output };
-
-    customGarbage[0] = input.ptr;
-    customGarbage[1] = response.output.ptr;
-
-    Mat drawing = Mat::zeros(3, 3, CV_8UC3);
-    drawing.release();
+    customGarbage[0] = inputBitmap;
+    customGarbage[1] = response.output;
 
     return response;
 }
 
 void freeMemory() {
   for (int i = 0; i < customGarbageCount; i++) {
-    if (customGarbage[i] != 0) {
-      uintptr_t ptr = customGarbage[i]; 
-      void* castedPtr = reinterpret_cast<void *>(ptr);
-      free(castedPtr);
-    }
+    Bitmap4C_t bitmap = customGarbage[i]; 
+    bitmap.release();
   }
 }
 
